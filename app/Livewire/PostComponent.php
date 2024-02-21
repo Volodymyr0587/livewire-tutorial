96c5014203cfdb2d30b3dcd4d59ac77ed429e501
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-02-21 16:42:38 +0200

    Add validation rules to PostComponent

--- app/Livewire/PostComponent.php
+++ app/Livewire/PostComponent.php
@@ -3,10 +3,17 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Rule;
 
 class PostComponent extends Component
 {
     public $isOpen = 0;
+
+    #[Rule('required|min:3')]
+    public $title;
+
+    #[Rule('required|min:3')]
+    public $body;
 
     public function create()
     {
