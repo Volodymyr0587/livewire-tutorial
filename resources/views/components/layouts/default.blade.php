2204682d113901bbda43cabed7e943728d627bd2
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-02-21 15:52:52 +0200

    Create layout

--- resources/views/components/layouts/default.blade.php
+++
@@ -1,8 +0,0 @@
-<main>
-    @livewireStyles
-    <header>
-        <h2>Header</h2>
-    </header>
-    {{ $slot }} <!-- Here will go all content of pages that we are going to render -->
-    @livewireScripts
-</main>
--- resources/views/home.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layouts.default>
-    <h1>This is homepage</h1>
-    <livewire:create-product />
-</x-layouts.default>
