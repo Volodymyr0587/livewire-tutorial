e6a0774462bdef7e99a40a8b5650da234e28ff6a
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-02-21 16:24:27 +0200

    Create modal for add posts

--- app/Livewire/PostComponent.php
+++ app/Livewire/PostComponent.php
@@ -6,6 +6,22 @@ use Livewire\Component;
 
 class PostComponent extends Component
 {
+    public $isOpen = 0;
+
+    public function create()
+    {
+        $this->openModal();
+    }
+
+    public function openModal()
+    {
+        $this->isOpen = true;
+    }
+
+    public function closeModal()
+    {
+        $this->isOpen = false;
+    }
     public function render()
     {
         return view('livewire.post-component');
